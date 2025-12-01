08ef70b598cc02c732201343d7e2654061e8bdb2
Omar-Kamall <ok9192750000000@gmail.com>
2025-12-01 03:49:30 +0200

    Final Update CORS

--- Backend/app.js
+++ Backend/app.js
@@ -9,22 +9,20 @@ const contactRoutes = require("./routes/contact.routes");
 const orderRoutes = require("./routes/order.routes");
 const cartRoutes = require("./routes/cart.routes");
 
-
-
 dotenv.config();
 const app = express();
 
-
 // Configure CORS
-app.use(cors({
-  origin: "http://localhost:5173", // Frontend Link
-  credentials: true, // Send Tokens with Frontend
-}));
-
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Frontend Link
+    credentials: true, // Send Tokens with Frontend
+  })
+);
+app.use(cors());
 
 // Send Json
 app.use(express.json());
-
 
 // Accsses Port (.env)
 const port = process.env.PORT || 5050;
@@ -33,11 +31,11 @@ const port = process.env.PORT || 5050;
 connectDB();
 
 // routers uses
-app.use("/api/users" , userRoutes);
-app.use("/api" , productRoutes);
-app.use("/api/contact" , contactRoutes);
-app.use("/api/order" , orderRoutes);
-app.use("/api/cart" , cartRoutes);
+app.use("/api/users", userRoutes);
+app.use("/api", productRoutes);
+app.use("/api/contact", contactRoutes);
+app.use("/api/order", orderRoutes);
+app.use("/api/cart", cartRoutes);
 
 // listen port
 app.listen(port, () => {
